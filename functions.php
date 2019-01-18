6b797ffda436053937ea58cd86b70c3eb5af4e15
Alvaro Separovich <alvasepro@gmail.com>
2019-01-18 20:32:37 -0200

    product-page modified, order in side of product image

--- functions.php
+++ functions.php
@@ -46,5 +46,36 @@ function retrieve_var1_replacement( $especial_attribute=0, $all=0 ) {
 //-------
 //-------
 
+
+//=============================
+//---------product Page--------
+//=============================
+/** some hel with the default hooks order
+ * woocommerce_single_product_summary hook
+ *
+ * @hooked woocommerce_template_single_title - 5
+ * @hooked woocommerce_template_single_rating - 10
+ * @hooked woocommerce_template_single_price - 10
+ * @hooked woocommerce_template_single_excerpt - 20
+ * @hooked woocommerce_template_single_add_to_cart - 30
+ * @hooked woocommerce_template_single_meta - 40
+ * @hooked woocommerce_template_single_sharing - 50
+ */
 remove_action( "woocommerce_single_product_summary", "woocommerce_template_single_rating");
 add_action( "woocommerce_single_product_summary", "woocommerce_template_single_rating", 1);
+remove_action( "woocommerce_single_product_summary", "woocommerce_template_single_price");
+add_action( "woocommerce_single_product_summary", "woocommerce_template_single_price", 15);
+
+function start_price_block(){  echo"<div class='group-sumary'>";  return;};
+function start_p1_block(){  echo"<div class='group group-sumary-pt1'>";  return;};
+function start_p2_block(){  echo"<div class='group group-sumary-pt2'>";  return;};
+function end_block(){  echo"</div>";  return;};
+
+add_action( "woocommerce_single_product_summary", "start_price_block", 11);
+add_action( "woocommerce_single_product_summary", "end_block", 35);
+add_action( "woocommerce_single_product_summary", "end_block", 55);//start an oder block
+//add_action( "woocommerce_single_product_summary", "start_p1_block", 11);
+//add_action( "woocommerce_single_product_summary", "end_block", 25);
+add_action( "woocommerce_single_product_summary", "start_p2_block", 29);
+add_action( "woocommerce_single_product_summary", "end_block", 35);
+
