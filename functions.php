6be9325c8f7793030b731881c669110da2aec769
Alvaro Separovich <alvasepro@gmail.com>
2019-01-30 20:44:38 -0200

    Improve: loop aparence modified

--- functions.php
+++ functions.php
@@ -152,7 +152,7 @@ if ( ! function_exists( 'woocommerce_template_loop_product_title' ) ) {
   * Show the product H3 title in the product loop.
   */
   function woocommerce_template_loop_product_title() {
-  echo '<h3 class=”woocommerce-loop-product__title”>' . get_the_title() . '</h3>';
+  echo '<h3 class="woocommerce-loop-product__title">' . get_the_title() . '</h3>';
   if (wp_get_post_terms(get_post()->ID, "pa_autor") and ! is_wp_error( wp_get_post_terms(get_post()->ID, "pa_autor") ) ){
     echo '<h5>'. wp_get_post_terms(get_post()->ID, "pa_autor")[0]->name .'</h5>';  
   }else{
