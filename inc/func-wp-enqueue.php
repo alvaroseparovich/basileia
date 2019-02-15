851f3e8e750e018cd8366148ec96e5de7489e508
Alvaro Separovich <alvasepro@gmail.com>
2019-02-14 22:36:51 -0200

    oder changes

--- inc/func-wp-enqueue.php
+++ inc/func-wp-enqueue.php
@@ -10,8 +10,8 @@ function basileia_enqueue_styles_scripts() {
   wp_register_script('default-basileia-js', get_stylesheet_directory_uri().'/inc/js/default-basileia.js');
   wp_register_script('color-thief', get_stylesheet_directory_uri().'/inc/js/color-thief.min.js');
   //To enqueue scripts
-  wp_enqueue_script( 'color-thief','','','', true );
-  wp_enqueue_script( 'default-basileia-js','','','', true );
+  //wp_enqueue_script( 'color-thief','','','', true );
+  //wp_enqueue_script( 'default-basileia-js','','','', true );
   
 }
 add_action( 'wp_enqueue_scripts', 'basileia_enqueue_styles_scripts' );
