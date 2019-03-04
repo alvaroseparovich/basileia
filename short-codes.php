b1cfbcba0ec6dbc0b8d1769134d1e5803bd68816
Alvaro Separovich <alvasepro@gmail.com>
2019-03-04 11:08:35 -0300

    ShortCode to show list of brands

--- short-codes.php
+++ short-codes.php
@@ -49,3 +49,58 @@ function authors_shortcode( $atts ) {
 }
 
 add_shortcode( 'autores', 'authors_shortcode' );
+
+
+function brands_shortcode( $atts ) {
+    
+    $ordered_terms = ['a'=>"as"];
+    $ordered_terms = array(
+        'A' => array(),        'B' => array(),        'C' => array(),        'D' => array(),
+        'E' => array(),        'F' => array(),        'G' => array(),        'H' => array(),
+        'I' => array(),        'J' => array(),        'K' => array(),        'L' => array(),
+        'M' => array(),        'N' => array(),        'O' => array(),        'P' => array(),
+        'Q' => array(),        'R' => array(),        'S' => array(),        'T' => array(),
+        'U' => array(),        'V' => array(),        'W' => array(),        'X' => array(),
+        'Y' => array(),        'Z' => array(),        '#' => array()
+    );
+    
+    $terms = get_terms( array(
+        'taxonomy'      => 'pa_editora',
+        'hide_empty'    => false,
+        'order'         => 'ASC',
+    ) );
+
+    foreach ($terms as $key => $term) {
+
+        $letter = substr(strtoupper($term->slug),0,1);
+
+        if (strpos("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", $letter)){
+            array_push($ordered_terms[$letter],$term);
+        }else{
+            array_push($ordered_terms["#"],$term);
+
+        }
+    }
+
+    $string = "";
+    $string = $string . '<div class="letra-editora '.$key.'">';
+    foreach($ordered_terms as $key => $array_brands){
+        $string = $string .'<div class="Letter"><h4 class="letra">'.$key.'</h4>';
+        $string = $string . '<ul class="lista-letra-editora">';
+        foreach($array_brands as $k => $brand){
+            $at_link = esc_url( get_term_link( $brand, $brand->name ) ) ;
+            echo "<pre>";
+            $dump = var_dump($brand->meta_value);
+            echo "</pre>";
+            $string = $string . '<a href="'.$at_link.'"><li class="nome-editora">'.$dump.'</li></a>';
+/*             $string = $string . '<a href="'.$at_link.'"><li class="nome-editora">'.$brand->name.'</li></a>';
+ */
+        }
+        $string = $string . '</ul></div>';
+    }
+    $string = $string . '</div>';
+
+	return $string;
+}
+
+add_shortcode( 'editoras', 'brands_shortcode' );
