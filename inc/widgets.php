7040f90f177899ebe3d9482a758856116751db04
Alvaro Separovich <alvasepro@gmail.com>
2019-03-08 22:16:04 -0300

    -FEATURE: Widget to show img as Banner

--- inc/widgets.php
+++ inc/widgets.php
@@ -6,9 +6,11 @@
 add_action( 'widgets_init', 'basileia_widgets_init' );
 function basileia_widgets_init() {
 	register_widget( "attribute_feature_right" );
+	register_widget( "banner_img" );
 }
 
 function hw_arq($param=""){
     return basileia_dir().'/inc/widgets/'.$param;
 }
-require hw_arq('attribute_feature_right.php');
+require hw_arq('wd_attribute_feature_right.php');
+require hw_arq('wd_banner.php');
---
+++ inc/widgets/wd_banner.php
@@ -0,0 +1,133 @@
+<?php
+// Elshaddai banner com imagem
+
+
+
+class banner_img extends WP_Widget {
+	function __construct() {
+		$widget_ops = array(
+					'classname'   => 'banner collection-wrapper clearfix',
+					'description' => esc_html__( 'Display a responsive banner.', 'basileia' ) );
+                    $control_ops = array(
+                        'width'  => 200,
+                        'height' => 250,
+			);
+		parent::__construct( false, $name = esc_html__( 'A-C: banner', 'basileia' ), $widget_ops, $control_ops);
+	}
+
+	function form( $instance ) {
+
+		$defaults[ 'attr_link1' ]   = '';
+		$defaults[ 'attr_image_1' ]  = '';
+		$defaults[ 'attr_image_2' ]  = '';
+		$defaults[ 'lazy' ]  = '';
+		$defaults[ 'bg_color' ]  = '';
+
+		$instance = wp_parse_args( (array) $instance, $defaults );
+
+		$attr_link1 = esc_url( $instance[ 'attr_link1' ] );
+		$attr_image_1 = esc_url( $instance[ 'attr_image_1' ] );
+		$attr_image_2 = esc_url( $instance[ 'attr_image_2' ] );
+		$lazy = $instance[ 'lazy' ];
+		$bg_color = $instance[ 'bg_color' ];
+		?>
+		<label><h3><?php esc_html_e( 'Add your First Image here.', 'estore' ); ?></h3></label>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'lazy' ); ?>"> <?php esc_html_e( 'Carregamento tardío', 'basileia' ); ?></label>
+			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'lazy' ); ?>" name="<?php echo $this->get_field_name( 'lazy' ); ?>" <?php checked( $instance[ 'lazy' ], 'on' ); ?>/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'bg_color' ); ?>"> <?php esc_html_e( 'Cor do Background', 'estore' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'bg_color' ); ?>" name="<?php echo $this->get_field_name( 'bg_color' ); ?>" value="<?php echo $instance[ 'bg_color' ]; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'attr_link1' ); ?>"> <?php esc_html_e( 'Link', 'estore' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'attr_link1' ); ?>" name="<?php echo $this->get_field_name( 'attr_link1' ); ?>" value="<?php echo $instance[ 'attr_link1' ]; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'attr_image_1' ); ?>"> <?php esc_html_e( 'Image full', 'estore' ); ?></label>
+			<div class="media-uploader" id="<?php echo $this->get_field_id( 'attr_image_1' ); ?>">
+				<div class="custom_media_preview">
+					<?php if ( $instance[ 'attr_image_1' ] != '' ) : ?>
+						<img class="custom_media_preview_default" src="<?php echo esc_url( $instance[ 'attr_image_1' ] ); ?>" style="max-width:100%;" />
+					<?php endif; ?>
+				</div>
+				<input  type="text" class="widefat custom_media_input" id="<?php echo $this->get_field_id( 'attr_image_1' ); ?>" name="<?php echo $this->get_field_name( 'attr_image_1' ); ?>" value="<?php echo esc_url( $instance['attr_image_1'] ); ?>" style="margin-top:5px;" />
+				
+			</div>
+		</p>
+		<hr>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'attr_image_2' ); ?>"> <?php esc_html_e( 'Image mobile', 'estore' ); ?></label>
+			<div class="media-uploader" id="<?php echo $this->get_field_id( 'attr_image_2' ); ?>">
+				<div class="custom_media_preview">
+					<?php if ( $instance[ 'attr_image_2' ] != '' ) : ?>
+						<img class="custom_media_preview_default" src="<?php echo esc_url( $instance[ 'attr_image_2' ] ); ?>" style="max-width:100%;" />
+					<?php endif; ?>
+				</div>
+				<input type="text" class="widefat custom_media_input" id="<?php echo $this->get_field_id( 'attr_image_2' ); ?>" name="<?php echo $this->get_field_name( 'attr_image_2' ); ?>" value="<?php echo esc_url( $instance['attr_image_2'] ); ?>" style="margin-top:5px;" />
+				
+			</div>
+		</p>
+		<?php
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$instance[ 'attr_link1' ] = esc_url_raw( $new_instance[ 'attr_link1' ]);
+		$instance[ 'attr_image_1' ] = esc_url_raw( $new_instance[ 'attr_image_1' ] );
+		$instance[ 'attr_image_2' ]  = esc_url_raw( $new_instance[ 'attr_image_2' ] );
+		$instance[ 'lazy' ]  = $new_instance[ 'lazy' ];
+		$instance[ 'bg_color' ]  = $new_instance[ 'bg_color' ];
+
+		return $instance;
+	}
+
+	function widget( $args, $instance ) {
+		extract( $args );
+		extract( $instance );
+
+		global $post;
+
+		$attr_link1   = isset( $instance[ 'attr_link1' ] ) ? $instance[ 'attr_link1' ] : '';
+		$attr_image_1   = isset( $instance[ 'attr_image_1' ] ) ? $instance[ 'attr_image_1' ] : '';
+		$attr_image_2    = isset( $instance[ 'attr_image_2' ] ) ? $instance[ 'attr_image_2' ] : '';
+		$bg_color    = isset( $instance[ 'bg_color' ] ) ? $instance[ 'bg_color' ] : '';
+		$lazy    = $instance[ 'lazy' ]? 'true' : 'false';
+
+		$img_Pathern = get_stylesheet_directory_uri() . '/none.jpg';
+		echo $before_widget; ?>
+			<?php
+
+				?>
+				<div class="collection-block banner-adaptive">
+					<a href="<?php echo esc_url( $attr_link1 ); ?>" target="_blank">
+					<figure class="banner-adaptive-img" style="background:<?=$bg_color?>;">
+					<?php
+					if(!$lazy){
+						?>
+						<picture>
+						  <source media="(min-width: 600px)" srcset="<?=esc_url( $attr_image_1)?>">
+						  <img src="<?=esc_url( $attr_image_2 )?>" alt="Flowers" style="width:auto;">
+						</picture>
+						<?php
+					}else{
+						if ( $attr_image_1 and $attr_image_2 ) {
+                            /*$picture = "<picture>";
+                            $first_source = "<source media='(max-width:550px)' src='{$attr_image_1}'/>";
+                            $second_source = "<source media='(min-width:551px)' src='{$attr_image_2}'/>";
+                            $img_close = "<img src='{$attr_image_2}'></picture>";
+                            echo "{$picture}{$first_source}{$second_source}{$img_close}";*/
+                            
+                            echo "<img class='fll' src='{$attr_image_1}'/> <img class='mb' src='{$attr_image_2}'/>";
+					}}
+					?>
+					</figure>
+				</a>
+				</div>
+
+			<?php
+		echo $after_widget;
+	}
+}
